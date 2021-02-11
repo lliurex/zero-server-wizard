619fce1c4caeed6e730ef25789695bd25f683708
cless <hectorgh@gmail.com>
2021-02-11 10:47:22 +0100

    changed function name

--- install-files/usr/share/zero-server-wizard/types/independent/actions/015-network.py
+++ install-files/usr/share/zero-server-wizard/types/independent/actions/015-network.py
@@ -82,7 +82,7 @@ if ret[0]:
 		
 		print(c.set_nat(user,'NetworkManager', True, True, external_iface))
 		print(c.set_routing(user,'NetworkManager', True, True))
-		print(c.systemd_resolved_conf(user,"NetworkManager"))
+		print(c.systemd_resolv_conf(user,"NetworkManager"))
 		print(c.apply_changes(user,"NetworkManager"))
 		print(c.check_devices(user,"NetworkManager",[internal_iface,external_iface],60))
 		
